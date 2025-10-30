5f8a2f9134d7198f2341844bd6646e064b2a5ccb
Satoru Komatsu <skomatsu@c3r3s5.42tokyo.jp>
2025-10-30 18:52:28 +0900

    push

--- filter/filter.c
+++ filter/filter.c
@@ -1,4 +1,29 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+#define BUFFER_SIZE 4096
+
+
+int main(int argc, char **argv)
+{
+    char *pattern;
+    size_t pattern_len;
+    char *read_buffer;
+    char *overlap_buffer;
+    size_t overlap_size;
+    char *work_buffer;
+    size_t work_size;
+    ssize_t bytes_read;
+    size_t safe_size;
+
+    if (argc != 2)
+        return (1);
+    
+    pattern = argv[1];
+    pattern_len = str
+    
+}
